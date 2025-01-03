547b4e3923f26603e9ffb3ced157b7fab71e2b1d
Dev Malhotra <153187417+DevM12@users.noreply.github.com>
2025-01-04 01:35:42 +0530

    Final Commit

---
+++ Day-25(US state guessing game)/U.S._States_game/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle,Screen,mainloop
+import pandas
+
+data=pandas.read_csv('50_states.csv')
+states=data['state'].tolist()
+
+screen=Screen()
+screen.title('U.S. States game')
+
+img='blank_states_img.gif'
+screen.addshape(img)
+tim=Turtle(img)
+
+
+s=Turtle()
+s.hideturtle()
+s.penup()
+
+guessed_states=[]
+count=0
+cont_game=True
+while cont_game and len(guessed_states)<50:
+    answer_state=screen.textinput(f'Guessed states:{count}/50','Write name of the state').title()
+    if answer_state=='Exit':
+        cont_game=False
+    if (answer_state in states) and (answer_state not in guessed_states):
+        state=data[data.state==answer_state]
+        guessed_states.append(answer_state)
+        x,y=state.x.item(),state.y.item()
+        s.goto((x,y))
+        s.write(answer_state)
+        count+=1
+if count<50:
+    missed_states=[]
+    for s in states:
+        if s not in guessed_states:
+            missed_states.append(s)
+            new_data=pandas.DataFrame(missed_states)
+            new_data.to_csv('states_to_learn.csv')
+mainloop()
